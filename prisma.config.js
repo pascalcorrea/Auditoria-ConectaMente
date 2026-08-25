fa4a0421877eb4ad3bc1a667c82acd354a4cb70f
pascalcorrea <pcv165@gmail.com>
2026-08-25 15:40:52 -0400

    fix: Prisma v7 config must be empty, URL only in PrismaClient

--- prisma.config.js
+++ prisma.config.js
@@ -1,4 +1,2 @@
-/** @type {import('@prisma/internals').GetConfig} */
-module.exports = {
-  datasourceUrl: () => process.env.DATABASE_URL,
-}
+// Prisma v7 config - URL managed in lib/prisma.ts via PrismaClient adapter
+module.exports = {}
